599ec9b0f91565d7501f94fc2a51a4578ceb6dcd
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2026-07-21 11:01:18 +0200

    remove `OnPush`

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { MacroQuestShell } from './macroquest/ui/macroquest-shell';
 
 @Component({
   selector: 'app-root',
   imports: [MacroQuestShell],
   templateUrl: './app.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
